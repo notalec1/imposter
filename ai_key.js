3c3ef8cbda48503801e765291e47bf4f7a16f7aa
notalec1 <alecghorayeb.ag@gmail.com>
2025-12-16 17:13:56 +0200

    Update ai_key.js
    use model gemini-1.5-flash-001

--- ai_key.js
+++ ai_key.js
@@ -1,5 +1,7 @@
 // ai_key.js
 const AI_CONFIG = {
     API_KEY: "AIzaSyDmO6ZZHr_4GaQKQpRfI1jcPNKyU_l5ar0", 
-    TYPE: "google" // This tells the game to use Google logic
+    TYPE: "google", // This tells the game to use Google logic
+    // If this fails, try "gemini-pro" or "gemini-1.5-flash-latest"
+    MODEL: "gemini-1.5-flash-001" 
 };
